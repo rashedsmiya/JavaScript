5d52121b0e8c6111a460476f9fed6e1f285fd8da
rashedmiya33 <132655219+rashedmiya33@users.noreply.github.com>
2023-05-06 20:38:39 +0600

    1 file added

---
+++ JavaScript/Objects.js
@@ -0,0 +1,34 @@
+// Amigosecode
+
+var person = {
+  firstName: 'Jamila',
+  age: 21,
+  isFemale: true,
+  balance: 100.32,
+  dob: new Date(2000, 1, 28).toJSON(),
+  address: {
+    city: 'London',
+    postCode: 'SW9',
+  },
+};
+
+console.log(person.firstName);
+console.log(person.age);
+console.log(person.balance);
+console.log(person.address.city);
+console.log(Object.values(person));
+console.log(Object.keys(person));
+console.log(JSON.stringify(person));
+
+//Asraful Haq
+
+const student = [
+  [01, 'Asraf', 20, 'Mirpur'],
+  [02, 'Jillur', 10, 'uttora'],
+  [03, 'Rahim', 120, 'Gulsan'],
+  [04, 'Karim', 200, 'Danmondi'],
+];
+
+for (let i = 0; i < student.length; i++) {
+  console.log(student[i]);
+}
