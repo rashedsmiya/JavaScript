cd5ccba6dd6703848d9af343f3b4f8c5accfb186
rashedmiya33 <132655219+rashedmiya33@users.noreply.github.com>
2023-05-06 09:17:17 +0600

    1 file modified, 2 added

---
+++ JavaScript/DataType.js
@@ -0,0 +1,214 @@
+// Amigoscode
+
+var firstName = 'jamila';
+var age = 21;
+var isFemale = true;
+var balance = 100.32;
+var dob = new Date(2000, 1, 28);
+var person = {};
+var empty = undefined;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isFemale);
+console.log(typeof balance);
+console.log(typeof dob);
+console.log(typeof person);
+console.log(typeof empty);
+
+// Coder's Gyan
+
+Primitive data types
+1. Number
+
+let age = 25;
+let temp = -25;
+let price = 50.50;
+
+2. String
+let name = 'Rakesh';
+3. Boolean
+true / false
+let isLoggedIn = true;
+let hasPermission = false;
+4. undefined
+let age;
+console.log(age);
+5.BigInt
+6.Symbol
+Unique
+
+2. Structural type
+1. Object
+1.1 Function
+Non-data structure
+callable
+1.2 Arrays
+collection of values
+const number = [2, 3, 4, 'Javascript', 'Pythone', { name: 'Rakesh' }];
+1.3 Maps
+1.4 Set
+1.5 Date
+
+3. Structural root
+1. null
+
+
+// Dev Dreamer
+
+let num = 7;
+let newNum = 7.58;
+
+let favFood = 'pizza';
+let favColor = 'purple';
+
+console.log(180 > 1);
+let js = 'cool';
+
+console.log(js == 'cool');
+
+let bigNumber = 9007199254740992n;
+console.log(typeof bigNumber);
+
+let channelName;
+console.log(typeof channelName);
+
+let myList = ['Amit', true, 'Dev Dreamer', 7];
+console.log(myList[3]);
+
+let hello = function () {
+  console.log(hello);
+};
+
+hello();
+hello();
+hello();
+
+let character = {
+  name: 'Sonic the hedgehog',
+  color: 'blue',
+  speed: 1000,
+  fast: true,
+};
+
+console.log(character.speed);
+
+let favDrink = 'Lemonade';
+let favNum = 7;
+
+let myFavs = function () {
+  console.log(favDrink, favNum);
+};
+
+myFavs();
+
+let person = {
+  name: 'John',
+  age: 30,
+};
+
+console.log(person);
+
+// Programiz
+
+console.log(`I love Javascript`);
+console.log(typeof `I love Javascript`);
+
+console.log('8');
+console.log(typeof '80.5');
+
+let language = 'JavaScript';
+console.log('language');
+
+let number = 5;
+console.log(number);
+
+var name = 'jacok';
+
+console.log(name);
+
+let name = 'punit';
+console.log(name);
+
+name = 'James';
+console.log(name);
+
+let name = 'Punit';
+let surname = 'Jajodia';
+
+name = surname;
+console.log(name);
+
+const passportNumber = 39983;
+console.log(passportNumber);
+
+passportNumber = 44325;
+console.log(passportNumber);
+
+let name;
+console.log(name);
+
+let name = 'Punit';
+console.log(name);
+
+name = undefined;
+console.log(name);
+
+let city = 'New York';
+
+console.log(`city:${city}`);
+
+let city = 'New York';
+let kfcLocations = 10;
+
+console.log(`City:' ${city}', kfcLocations ${kfcLocations}`);
+
+
+
+// w3schools
+
+let carName1 = 'Volvo XC60'; // Double quotes
+let carName2 = 'Volvo XC60'; // Single quotes
+
+// String
+
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+
+console.log(answer1);
+
+// Number
+
+let x1 = 34.0;
+let x2 = 34;
+let x3 = 3.14;
+
+console.log(x1, x2);
+
+// Boolean
+
+let z = 5;
+let y = 5;
+let x = 6;
+
+console.log(y == z, x == y);
+
+// Array
+const cars = ['Saab', 'Volvo', 'BMW'];
+
+console.log(cars[2]);
+
+// Objects
+
+const person = {
+  firstName: 'John',
+  last_Name: 'Doe',
+  age: 50,
+  eyeColor: 'blue',
+};
+
+console.log(person.age);
+
+let car;
+console.log(typeof car);
---
+++ JavaScript/Variables.js
@@ -0,0 +1,92 @@
+// Amigose Code
+
+var firstName = "jamila";
+var age = 21;
+var isFemale = true;
+var balance = 100.32;
+var dob = new Date(2000, 1, 28);
+var person = {}
+var empty = undefined
+
+console.log(firstName);
+console.log(age);
+console.log(isFemale);
+console.log(balance);
+console.log(dob);
+console.log(person);
+console.log(empty);
+
+
+// Code with Harry
+
+console.log('tut3');
+// Variables in js
+// var, let, const
+var name = 'harry';
+var channel;
+var marks = 3454;
+// channel = 'CodeWithHarry'
+console.log(name, channel, marks)
+// Rules for creating JavaScript Variables
+
+
+// 1. Cannot start with numbers 
+// 2. Can start with letter, numbers, _ or $ 
+// 3. Are Case sensitive 
+
+
+var city = 'Delhi'
+console.log(city)
+
+const owwnersName = 'Hari Ram';
+// owwnersName = 'Harry'; // Cannot do this (error)
+console.log(owwnersName);
+const fruit = 'Orange';
+
+{
+    let city = 'Rampur';
+    city = 'kolkata';
+    console.log(city);
+}
+console.log(city);
+
+const arr1 = [12,23,12,53, 3];
+// arr1.push(45);
+console.log(arr1)
+
+// Asraful Haque
+
+var name = 'Asraful Haque';
+var food = 'Bereyani';
+
+console.log('My name is '+ name +' & I love to eat ' + food);
+
+var name;
+
+name = 'kibria';
+
+name = 'jamal';
+
+var name = 'koddos';
+
+name = 'koddos';
+
+console.log("I am " + name);
+
+let name = 'Raju';
+
+name = 'Looli';
+name = 'Mitu';
+name = 'Mina';
+
+let name = 'Raju'; (error)
+
+console.log("I am " + name);
+
+const name = "Asarf";
+console.log("I am " + name);
+
+    let _naam = 'kolim'
+    let _boyoss = '100';
+
+console.log("I am " + _naam + " & I am " + _boyoss + " years old");
