d7ce56804a51f8f053849c94be5a0c8333ff49ff
rashedsmiya <rashedmiyaofficial@gmail.com>
2024-06-15 13:32:33 +0600

    TEST

---
+++ app.js
@@ -0,0 +1,7 @@
+echo "# jQuery" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rashedsmiya/jQuery.git
+git push -u origin main
